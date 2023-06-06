d085e2b7de903f5ed5877d14d6df4930708271e8
Serhii Neliubov <84928517+JakeJenneth@users.noreply.github.com>
2023-06-06 22:06:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+const requestURL = 'https://swapi.dev/api/people/?format=json';
+
+function sendRequest(method, url) {
+   return fetch(url)
+      .then(response => response.json())
+      .then(data => data.results)
+      .catch(err => console.log(err))
+}
+
+sendRequest('GET', requestURL)
+   .then(people => processPeople(people))
+   .catch(err => console.log(err))
+
+
+const table = document.querySelector('#table')
+const pagination = document.querySelector('#pagination')
+const notesOnPage = 3
+const items = []
+
+function processPeople(people) {
+   for(let i = 1; i <= Math.ceil(people.length/notesOnPage); i++){
+      let li = document.createElement('li')
+      li.innerHTML = i;
+      pagination.appendChild(li)
+      items.push(li)
+   }
+
+   showPage(items[0])
+
+   for (let item of items) {
+      item.addEventListener('click', function() {
+         showPage(this)
+      })
+   }
+
+
+   function showPage(item) {
+      let active = document.querySelector('#pagination li.active')
+      if (active){
+         active.classList.remove('active')
+      }
+      
+      item.classList.add('active')
+
+      const pageNum = +item.innerHTML
+      const start = (pageNum - 1) * notesOnPage
+      const end = start + notesOnPage
+      const notes = people.slice(start, end)
+
+      table.innerHTML = '';
+      for (let note of notes){
+         let tr = document.createElement('tr')
+         table.appendChild(tr)
+
+         createCell(note.name, tr)
+         createCell(note['eye_color'], tr)
+         createCell(note.gender, tr)
+      }
+   }
+
+   function createCell(text, tr){
+      let td = document.createElement('td')
+      td.innerHTML = text
+      table.appendChild(td)
+   }
+}
